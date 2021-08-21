09aa41dae93a56a2a54fc8ea98c57108f488aa76
Miguel Angel Tomairo <migueltomairo@bcp.com.pe>
2021-08-21 02:51:10 -0500

    first commit

---
+++ applePayButton/CardTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  CardTableViewCell.swift
+//  applePayButton
+//
+//  Created by miguel tomairo on 8/21/21.
+//
+
+import UIKit
+import PassKit
+
+class CardTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cardNumber: UILabel!
+    @IBOutlet weak var cardStack: UIStackView!
+    @IBOutlet weak var appleStack: UIStackView!
+    
+    let button = PKAddPassButton()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        button.addPassButtonStyle = .blackOutline
+        button.addTarget(self, action: #selector(passTouchUpInside), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let scale = CGFloat(floatLiteral: 1)
+        button.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+        appleStack.addArrangedSubview(button)
+    }
+    
+    @objc func passTouchUpInside() {
+        print("presiono")
+        print("ancho \(button.frame.size.width)")
+        print("alto \(button.frame.size.height)")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
---
+++ applePayButton/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  applePayButton
+//
+//  Created by miguel tomairo on 8/19/21.
+//
+
+import UIKit
+import PassKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cards = ["4242 **** **** 7483", "4242 **** **** 7483", "4242 **** **** 7483"]
+    let names = ["Pedro Silva", "Miguel Grau Seminario", "Francisco Bolognesi Cervantes"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        overrideUserInterfaceStyle = .light
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    
+    private func setupAddPassButton() {
+        
+//        let button = PKAddPassButton()
+//        button.addPassButtonStyle = .blackOutline
+//        button.addTarget(self, action: #selector(passTouchUpInside), for: .touchUpInside)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        let scale = CGFloat(floatLiteral: 0.8)
+//        button.transform = CGAffineTransform(scaleX: scale, y: scale)
+//        fondoView.addSubview(button)
+//        button.centerYAnchor.constraint(equalTo: fondoView.centerYAnchor).isActive = true
+//        button.trailingAnchor.constraint(equalTo: fondoView.trailingAnchor, constant: -5).isActive = true
+
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CardCell", for: indexPath) as! CardTableViewCell
+        
+        cell.nameLabel.text = names[indexPath.row]
+        cell.cardNumber.text = cards[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 76
+    }
+}
+
+
+// MARK: VALORES
+
+// iphone SE(second): ancho 142 alto 46
+// iphone 7 : ancho: ancho 142 alto 46
+// iphone 7 plus : ancho 141,66666 alto: 45.3333
+// iphone 11: ancho 142 alto 46
+
+// MARK: iphone fisico
+// iphone 11 pro: ancho 187 alto 50
+// iphone x: ancho 186.333333 alto 46
